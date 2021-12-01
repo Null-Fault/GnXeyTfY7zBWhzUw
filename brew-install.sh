5e55f674c0be2a8ae2070c652d967587a17c682c
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2021-12-01 02:08:05 -0700

    Initial

---
+++ brew-install.sh
@@ -0,0 +1,24 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew tap homebrew/core
+brew tap homebrew/cask
+brew tap homebrew/cask-drivers
+brew tap microsoft/git
+
+brew install git
+brew install iperf3
+
+brew install --cask firefox
+brew install --cask microsoft-teams
+brew install --cask git-credential-manager-core
+brew install --cask powershell
+brew install --cask google-chrome
+brew install --cask rectangle
+brew install --cask jabra-direct
+brew install --cask visual-studio-code
+brew install --cask keepassxc
+brew install --cask vlc
+brew install --cask keka
+brew install --cask vmware-fusion
+brew install --cask kodi
+brew install --cask docker
