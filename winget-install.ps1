152179be1551de838b4ba9777a802529d6467bbd
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2021-12-04 19:19:29 -0700

    Initial

---
+++ winget-install.ps1
@@ -0,0 +1,7 @@
+winget install -e --id Mozilla.Firefox
+winget install -e --id Microsoft.Powershell
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Valve.Steam
+winget install -e --id ParsecCloudInc.Parsec
+winget install -e --id KeePassXCTeam.KeePassXC
+winget install -e --id Git.Git
