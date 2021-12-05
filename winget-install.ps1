198275d886dca8d43ef054c436025ab9c94fc4c3
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2021-12-04 19:55:21 -0700

    Corrected Parsec

--- winget-install.ps1
+++ winget-install.ps1
@@ -2,6 +2,6 @@ winget install -e --id Mozilla.Firefox
 winget install -e --id Microsoft.Powershell
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Valve.Steam
-winget install -e --id ParsecCloudInc.Parsec
+winget install -e --id Parsec.Parsec
 winget install -e --id KeePassXCTeam.KeePassXC
 winget install -e --id Git.Git
