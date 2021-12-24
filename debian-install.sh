383ed73b77c220ae64518787478db10458a29461
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2021-12-24 01:37:45 -0700

    Initial

---
+++ debian-install.sh
@@ -0,0 +1,7 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install vlc
+sudo apt install keepassxc
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
