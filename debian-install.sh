f426ae9e721e97f8f6cce6c26bd0d3fd15115d9d
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2021-12-25 01:53:27 -0700

    Code, Parsec, Chrome added.

--- debian-install.sh
+++ debian-install.sh
@@ -2,6 +2,14 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install vlc
 sudo apt install keepassxc
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' -O chrome.deb
+sudo apt install ./chrome.deb
+rm ./chrome.deb
 
+wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O vscode.deb
+sudo apt install ./vscode.deb
+rm ./vscode.deb
+
+wget 'https://builds.parsecgaming.com/package/parsec-linux.deb' -O parsec.deb
+sudo apt install ./parsec.deb
+rm ./parsec.deb
