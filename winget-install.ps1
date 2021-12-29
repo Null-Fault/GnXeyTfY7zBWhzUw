ee85bd0ed8231735a7f16c24845935ee40e2959d
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2021-12-29 16:56:40 -0700

    Added Chrome

--- winget-install.ps1
+++ winget-install.ps1
@@ -4,4 +4,5 @@ winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Valve.Steam
 winget install -e --id Parsec.Parsec
 winget install -e --id KeePassXCTeam.KeePassXC
+winget install -e --id Google.Chrome
 winget install -e --id Git.Git
