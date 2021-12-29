1ca6ddb52c21655e4d09dfdaa0b869814ecfe97e
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2021-12-29 16:59:45 -0700

    Update winget-install.ps1

--- winget-install.ps1
+++ winget-install.ps1
@@ -1,8 +1,17 @@
+#Browsers
 winget install -e --id Mozilla.Firefox
-winget install -e --id Microsoft.PowerShell
-winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Valve.Steam
-winget install -e --id Parsec.Parsec
-winget install -e --id KeePassXCTeam.KeePassXC
 winget install -e --id Google.Chrome
+
+#Dev
+winget install -e --id Microsoft.PowerShell
 winget install -e --id Git.Git
+winget install -e --id Microsoft.VisualStudioCode
+
+#Utilities
+winget install -e --id KeePassXCTeam.KeePassXC
+
+#Gaming
+winget install -e --id Valve.Steam
+
+#Optional
+winget install -e --id Parsec.Parsec
