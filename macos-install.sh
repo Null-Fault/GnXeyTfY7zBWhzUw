be433f8fdf79b5f7ac98bf4e4bd1cfab11540c62
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2022-08-25 02:37:27 -0600

    Rearranged a bit

--- macos-install.sh
+++ macos-install.sh
@@ -1,29 +1,32 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew tap homebrew/core
+brew tap microsoft/git
 brew tap homebrew/cask
 brew tap homebrew/cask-drivers
-brew tap microsoft/git
+brew tap homebrew/core
 
 brew install git
 brew install iperf3
 
 brew install --cask firefox
+brew install --cask google-chrome
 brew install --cask microsoft-teams
+brew install --cask microsoft-remote-desktop
 brew install --cask git-credential-manager-core
 brew install --cask powershell
-brew install --cask google-chrome
-brew install --cask rectangle
-brew install --cask jabra-direct
 brew install --cask visual-studio-code
-brew install --cask keepassxc
-brew install --cask vlc
+brew install --cask rectangle
+brew install --cask macs-fan-control
 brew install --cask keka
-brew install --cask vmware-fusion
+brew install --cask keepassxc
 brew install --cask kodi
-brew install --cask docker
-brew install --cask nvidia-geforce-now
-brew install --cask sidequest
+brew install --cask vlc
 brew install --cask steam
+brew install --cask nvidia-geforce-now
 brew install --cask parsec
-brew install --cask microsoft-remote-desktop
+
+# Maybe
+#brew install --cask jabra-direct
+#brew install --cask vmware-fusion
+#brew install --cask docker
+#brew install --cask sidequest
