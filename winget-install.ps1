4d6214fb515c3dd46f6e4815a9bbb58690f298f6
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2022-08-28 11:30:59 -0700

    20220828

--- winget-install.ps1
+++ winget-install.ps1
@@ -1,3 +1,6 @@
+winget install -e --id Microsoft.VC++2015-2019Redist-x64
+winget install -e --id Microsoft.RemoteDesktopClient
+
 #Browsers
 winget install -e --id Mozilla.Firefox
 winget install -e --id Google.Chrome
@@ -9,9 +12,11 @@ winget install -e --id Microsoft.VisualStudioCode
 
 #Utilities
 winget install -e --id KeePassXCTeam.KeePassXC
+winget install -e --id VideoLAN.VLC
 
 #Gaming
 winget install -e --id Valve.Steam
 
 #Optional
 winget install -e --id Parsec.Parsec
+winget install -e --id Nvidia.GeForceNow
