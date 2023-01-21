4cae6bc88a295fa415bf4a1f76693e0b8cbad846
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2023-01-21 13:46:40 -0700

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -1,3 +1,10 @@
 sambausername=samba
+
 adduser --no-create-home --disabled-password --disabled-login $sambausername
 smbpasswd -a $sambausername
+groupadd -g 1111 media
+usermod -aG media $sambausername
+
+share=//255.255.255.255/Share
+mnt=/mnt/samba
+mount -t cifs -o rw,username=$sambausername,uid=1000,gid=1111 $share $mnt
