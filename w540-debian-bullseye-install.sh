ad9acf8baf97673f502fc87c42f5c2f47d43122c
GnXeyTfY7zBWhzUw <88598710+GnXeyTfY7zBWhzUw@users.noreply.github.com>
2023-02-05 18:34:10 -0700

    Update w540-debian-bullseye-install.sh

--- w540-debian-bullseye-install.sh
+++ w540-debian-bullseye-install.sh
@@ -3,7 +3,9 @@ sudo apt update && sudo apt upgrade -y
 
 # Add to sudo group and reboot for safe measure
 if [ $(groups $(whoami)| grep -c sudo) -eq 0 ]; then
-  su -l -c "apt update -y && apt install -y sudo && adduser $(whoami) sudo && reboot now"
+  echo -e "Login as root to install sudo and add your current user to sudo.\nPlease run the script again after reboot.\n"
+  read -p "Press [Enter] key to continue..."
+  su -l -c "apt update -y && apt install -y sudo && adduser $(whoami) sudo && reboot now "
 fi
 
 sudo rm /etc/apt/sources.list
@@ -91,17 +93,16 @@ sudo apt install -y /tmp/parsec.deb
 rm /tmp/parsec.deb
 
 # w540 specific
-
-# Remove intel xorg package
-sudo apt remove -y xserver-xorg-video-intel
-
-# Install Nvidia driver (470 series is best as it works with K1100M whereas later versions do not)
+# Install Nvidia GPU
 sudo apt install -y nvidia-driver firmware-misc-nonfree
 
 # For glxinfo and glxgears for testing GPU
 sudo apt install -y mesa-utils
 
-# Fix screen tearing
+# Remove intel xorg package
+sudo apt remove -y xserver-xorg-video-intel
+
+# Fixes screen tearing
 echo "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
 sudo update-initramfs -u
 
@@ -113,6 +114,62 @@ Name=Set Nvidia GpuPowerMizerMode
 Exec=nvidia-settings -a [gpu:0]/GpuPowerMizerMode=1
 EOF
 
+# https://wiki.debian.org/NVIDIA%20Optimus#Using_NVIDIA_GPU_as_the_primary_GPU
+# Configure X11 for only Nvidia GPU to be used (disable Optimus) as it provides a much better docked workstation experience.
+cat << EOF | sudo tee /etc/X11/xorg.conf
+Section "ServerLayout"
+    Identifier "layout"
+    Screen 0 "nvidia"
+    Inactive "intel"
+EndSection
+
+Section "Device"
+    Identifier "nvidia"
+    Driver "nvidia"
+    BusID "PCI:1:0:0"
+EndSection
+
+Section "Screen"
+    Identifier "nvidia"
+    Device "nvidia"
+    Option "AllowEmptyInitialConfiguration"
+    #Option "UseDisplayDevice" "none"
+EndSection
+
+Section "Device"
+    Identifier "intel"
+    Driver "modesetting"
+    BusID "PCI:0:2:0"
+    Option "AccelMethod"  "none"
+    #Option "TearFree" "True"
+    #Option "Tiling" "True"
+    #Option "SwapbuffersWait" "True"
+EndSection
+
+Section "Screen"
+    Identifier "intel"
+    Device "intel"
+EndSection
+EOF
+
+cat << EOF | tee ~/.xsessionrc:
+xrandr --setprovideroutputsource modesetting NVIDIA-0
+xrandr --auto
+xrandr --dpi 96
+EOF
+chmod +x ~/.xsessionrc
+
+# Gnome Display Manager configuration
+cat << EOF | sudo tee /usr/share/gdm/greeter/autostart/optimus.desktop
+[Desktop Entry]
+Type=Application
+Name=Optimus
+Exec=sh -c "xrandr --setprovideroutputsource modesetting NVIDIA-0; xrandr --auto"
+NoDisplay=true
+X-GNOME-Autostart-Phase=DisplayServer
+EOF
+
 # Steam
+sudo dpkg --add-architecture i386
 sudo apt install -y steam
 sudo apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
