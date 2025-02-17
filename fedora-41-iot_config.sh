91511746627684c8e4f4c638f4e619e03ac87672
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2025-02-16 19:31:59 -0700

    Update and rename fedora41iot-autoupdate.sh to fedora-41-iot_config.sh

--- fedora-41-iot_config.sh
+++ fedora-41-iot_config.sh
@@ -2,4 +2,5 @@ echo AutomaticUpdatePolicy=apply | sudo tee -a /etc/rpm-ostreed.conf
 sudo rpm-ostree reload
 sudo systemctl edit --force --full rpm-ostreed-automatic.timer # Change to however many days
 sudo systemctl enable rpm-ostreed-automatic.timer --now
+rpm-ostree kargs --delete-if-present='$ignition_firstboot'
 rpm-ostree status
