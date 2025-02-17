097ccb74e3f4b9a7224b035fc781b3d819e008f2
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2025-02-17 15:52:39 -0700

    Update fedora-38-silverblue_config.sh

--- fedora-38-silverblue_config.sh
+++ fedora-38-silverblue_config.sh
@@ -1,11 +1,16 @@
+# Upgrade ostree
+rpm-ostree upgrade --reboot
+
+# Update flatpaks
+flatpak update -y
+
+# Install new flatpaks
 flatpak install flathub org.mozilla.firefox
 flatpak install flathub com.google.Chrome
 flatpak install flathub org.keepassxc.KeePassXC
-flatpak install flathub com.valvesoftware.Steam
-flatpak install flathub com.github.iwalton3.jellyfin-media-player
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub com.visualstudio.code
 flatpak install flathub com.visualstudio.code.tool.podman
 
-rpm-ostree install gnome-shell-extension-dash-to-dock
-rpm-ostree install podman-docker podman-compose
+# flatpak install flathub com.valvesoftware.Steam
+# flatpak install flathub com.github.iwalton3.jellyfin-media-player
