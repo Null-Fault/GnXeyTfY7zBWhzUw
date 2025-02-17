d85776d993391d531a3db7cb104804b7ceb0a961
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2025-02-17 15:52:31 -0700

    Update fedora-41-iot_config.sh

--- fedora-41-iot_config.sh
+++ fedora-41-iot_config.sh
@@ -1,9 +1,5 @@
-# Update flatpaks
-flatpak update -y
-# Update rpm-opstree
 sudo rpm-ostree upgrade --reboot
-# Install qemu-guest-agent
-sudo rpm-ostree install --allow-inactive --assumeyes --reboot qemu-guest-agent
+
 # Configure auto uopdates
 echo AutomaticUpdatePolicy=apply | sudo tee -a /etc/rpm-ostreed.conf
 sudo rpm-ostree reload
@@ -12,5 +8,3 @@ sudo systemctl enable rpm-ostreed-automatic.timer --now
 # Disable zezere
 rpm-ostree kargs --delete-if-present='$ignition_firstboot' # https://github.com/fedora-iot/iot-distro/issues/14
 systemctl disable zezere_ignition.timer --now
-
-rpm-ostree status
