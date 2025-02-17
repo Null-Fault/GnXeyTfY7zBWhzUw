99f1166244c2bf3f201cd0d7907ee85f72be7bbc
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2025-02-16 18:49:48 -0700

    Create fedora41iot-autoupdate.sh

---
+++ fedora41iot-autoupdate.sh
@@ -0,0 +1,5 @@
+echo AutomaticUpdatePolicy=apply | sudo tee -a /etc/rpm-ostreed.conf
+sudo rpm-ostree reload
+sudo systemctl edit --force --full rpm-ostreed-automatic.timer # Change to however many days
+sudo systemctl enable rpm-ostreed-automatic.timer --now
+rpm-ostree status
