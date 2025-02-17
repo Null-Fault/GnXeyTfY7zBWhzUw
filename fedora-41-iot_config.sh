f86aa6a8f6da154f2e4f53fc90563dca328fe2bf
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2025-02-16 20:45:46 -0700

    Update fedora-41-iot_config.sh

--- fedora-41-iot_config.sh
+++ fedora-41-iot_config.sh
@@ -2,5 +2,6 @@ echo AutomaticUpdatePolicy=apply | sudo tee -a /etc/rpm-ostreed.conf
 sudo rpm-ostree reload
 sudo systemctl edit --force --full rpm-ostreed-automatic.timer # Change to however many days
 sudo systemctl enable rpm-ostreed-automatic.timer --now
+systemctl disable zezere_ignition.timer --now
 rpm-ostree kargs --delete-if-present='$ignition_firstboot' # https://github.com/fedora-iot/iot-distro/issues/14
 rpm-ostree status
