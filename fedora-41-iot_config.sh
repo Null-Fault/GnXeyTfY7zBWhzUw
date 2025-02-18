4fadbe7da395ce341c75ff5799e852852719f61c
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2025-02-17 19:06:57 -0700

    Update fedora-41-iot_config.sh

--- fedora-41-iot_config.sh
+++ fedora-41-iot_config.sh
@@ -8,9 +8,9 @@ echo AutomaticUpdatePolicy=apply | tee -a /etc/rpm-ostreed.conf
 rpm-ostree reload
 systemctl edit --force --full rpm-ostreed-automatic.timer # Change to however many days
 systemctl enable rpm-ostreed-automatic.timer --now
-# Disable zezere
-rpm-ostree kargs --delete-if-present='$ignition_firstboot' # https://github.com/fedora-iot/iot-distro/issues/14
-systemctl disable zezere_ignition.timer --now
+# # Disable zezere
+# rpm-ostree kargs --delete-if-present='$ignition_firstboot' # https://github.com/fedora-iot/iot-distro/issues/14
+# systemctl disable zezere_ignition.timer --now
 
 # Set static IP address and hostname
 list_interfaces() {
