d0f526733a5a7be20af9c72424944daf5a82a0af
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2025-12-23 13:37:09 -0700

    Update and rename fedora-38-silverblue_config.sh to fedora-43-silverblue_config.sh

---
+++ fedora-43-silverblue_config.sh
@@ -0,0 +1,23 @@
+# Upgrade ostree
+rpm-ostree upgrade --reboot
+
+# Install Distrobox because it's better than Toolbox
+# Layering isn't recommened but it's best if distrobox is layered
+rpm-ostree install distrobox
+
+# # Install qemu-guest-agent
+# rpm-ostree install --allow-inactive --assumeyes --reboot qemu-guest-agent
+
+# Update flatpaks
+flatpak update -y
+
+# Install new flatpaks
+# flatpak install flathub org.mozilla.firefox
+# flatpak install flathub com.google.Chrome
+# flatpak install flathub org.keepassxc.KeePassXC
+# flatpak install flathub com.github.tchx84.Flatseal
+# flatpak install flathub com.visualstudio.code
+# flatpak install flathub com.visualstudio.code.tool.podman
+
+# flatpak install flathub com.valvesoftware.Steam
+# flatpak install flathub com.github.iwalton3.jellyfin-media-player
