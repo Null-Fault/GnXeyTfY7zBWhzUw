ad372c77454e4fa0a336a7467a3f29ef2030e141
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2026-04-18 01:02:11 -0600

    Create bluefin-dx-43.sh

---
+++ bluefin-dx-43.sh
@@ -0,0 +1,12 @@
+# Flatpaks
+
+# Install Steam
+
+# Install RetroDeck 
+
+# RetroDeck can add itself to Steam
+# However the shortcut doesn't work
+# Change it to:
+# Target: /usr/bin/flatpak-spawn
+# Launch Options: --host flatpak run net.retrodeck.retrodeck
+
