af507f181083c9abe78170486f106aba0a67781c
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2026-04-18 23:16:11 -0600

    Update bluefin-dx-43.sh

--- bluefin-dx-43.sh
+++ bluefin-dx-43.sh
@@ -4,6 +4,9 @@
 
 # Install RetroDeck 
 
+
+flatpak override --user --talk-name=org.freedesktop.Flatpak com.valvesoftware.Steam
+flatpak override --user --talk-name=org.freedesktop.Flatpak net.retrodeck.retrodeck
 # RetroDeck can add itself to Steam
 # However the shortcut doesn't work
 # Change it to:
