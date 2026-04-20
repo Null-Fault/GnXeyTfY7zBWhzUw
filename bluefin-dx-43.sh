520a6a54b6bae496d8cb78975c58f39ae97d55bd
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2026-04-19 18:57:40 -0600

    Update bluefin-dx-43.sh

--- bluefin-dx-43.sh
+++ bluefin-dx-43.sh
@@ -35,7 +35,7 @@ flatpak override --user --system-talk-name=org.freedesktop.NetworkManager com.va
 # gamescope -f -H 2256 -S stretch -- %command%
 # gamescope -f -W 2256 -H 1504 -- %command%
 # For running Steam apps in Gamescope and MangoHud, use this in the launch properties of Steam:
-# gamescope -f -H 2256 -S stretch -- mangohud %command% 
+# gamescope -f -H 2256 -S stretch -- mangohud %command%
 # To enable MangoHud for all Steam games:
 # flatpak override --user --env=MANGOHUD=1 com.valvesoftware.Steam
 
