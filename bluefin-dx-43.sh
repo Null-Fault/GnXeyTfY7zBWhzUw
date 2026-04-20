91eca6b44fdb88ea3835ee5c1de5e58c78acb6fc
Null-Fault <88598710+Null-Fault@users.noreply.github.com>
2026-04-19 18:11:08 -0600

    Update bluefin-dx-43.sh

--- bluefin-dx-43.sh
+++ bluefin-dx-43.sh
@@ -1,3 +1,10 @@
+# Bluefin default Gnome scaling
+# org.gnome.mutter experimental-features ['scale-monitor-framebuffer', 'xwayland-native-scaling']
+# To set back
+# gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer', 'xwayland-native-scaling']"
+# Remove Gnome scaling for better compatibility
+gsettings set org.gnome.mutter experimental-features "[]"
+
 # Flatpaks
 
 # Install Steam
@@ -7,21 +14,14 @@ flatpak install flathub org.freedesktop.Platform.VulkanLayer.gamescope
 flatpak install flathub org.freedesktop.Platform.VulkanLayer.MangoHud
 # Install RetroDeck
 flatpak install flathub net.retrodeck.retrodeck
-
-# Bluefin default Gnome scaling
-# org.gnome.mutter experimental-features ['scale-monitor-framebuffer', 'xwayland-native-scaling']
-# To set back
-# gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer', 'xwayland-native-scaling']"
-# Remove Gnome scaling
-gsettings set org.gnome.mutter experimental-features "[]"
-
-#
-flatpak override --user --system-talk-name=org.freedesktop.NetworkManager com.valvesoftware.Steam
+# This is so Steam can start RetroDeck
 flatpak override --user --talk-name=org.freedesktop.Flatpak com.valvesoftware.Steam
+# This improves startup
+flatpak override --user --system-talk-name=org.freedesktop.NetworkManager com.valvesoftware.Steam
+
 # flatpak override --user --talk-name=org.freedesktop.Flatpak net.retrodeck.retrodeck
 # RetroDeck can add itself to Steam
 # However the shortcut doesn't work
 # Change it to:
 # Target: /usr/bin/flatpak-spawn
 # Launch Options: --host flatpak run net.retrodeck.retrodeck
-
